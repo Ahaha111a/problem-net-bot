2057715541bfb72361ef74c5d8fe5a2e909c9196
Ahaha111a <arkariys777@gmail.com>
2026-08-07 00:40:28 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -19,3 +19,33 @@ main_keyboard = ReplyKeyboardMarkup(
     resize_keyboard=True,
     input_field_placeholder="Выберите действие..."
 )
+
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+def moderation_keyboard():
+
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="✅ Опубликовать",
+                    callback_data="publish"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    text="✏️ Изменить",
+                    callback_data="edit"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    text="❌ Отклонить",
+                    callback_data="reject"
+                )
+            ]
+        ]
+    )
+
+    return keyboard2
