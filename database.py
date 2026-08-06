18cad974180be913f5803d12ba1d645052ceb38c
Ahaha111a <arkariys777@gmail.com>
2026-08-06 21:46:27 +0300

    Create database.py

---
+++ database.py
@@ -0,0 +1,41 @@
+import sqlite3
+
+
+connection = sqlite3.connect("stories.db")
+
+cursor = connection.cursor()
+
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS stories (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    text TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+""")
+
+
+connection.commit()
+
+
+def save_story(user_id: int, text: str):
+
+    cursor.execute(
+        """
+        INSERT INTO stories (user_id, text)
+        VALUES (?, ?)
+        """,
+        (user_id, text)
+    )
+
+    connection.commit()
+
+
+def get_all_stories():
+
+    cursor.execute(
+        "SELECT * FROM stories ORDER BY id DESC"
+    )
+
+    return cursor.fetchall()
