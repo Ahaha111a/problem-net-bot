42ccecca0a85a4b2603ce63769091b99d1406e80
Ahaha111a <arkariys777@gmail.com>
2026-08-06 21:19:53 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
+
+if not BOT_TOKEN:
+    raise ValueError("Не найден BOT_TOKEN")
+
+if not ADMIN_ID:
+    raise ValueError("Не найден ADMIN_ID")
