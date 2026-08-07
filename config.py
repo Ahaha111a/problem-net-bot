d243512e66bd01d3b7a0d6d6a1082d125d723f22
Ahaha111a <arkariys777@gmail.com>
2026-08-08 01:58:06 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,41 @@
 import os
+
 from dotenv import load_dotenv
 
-load_dotenv()  # Загружает переменные из .env
 
-class Config:
-    BOT_TOKEN: str = os.getenv("BOT_TOKEN")
-    DB_PATH: str = "stories.db"
-    CHANNEL_ID: int = 0
-    ADMIN_IDS: list[int] = [int(x) for x in os.getenv("ADMIN_IDS", "").split(",") if x]
+load_dotenv()
 
-config = Config()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+ADMIN_IDS = [
+    int(admin_id.strip())
+    for admin_id in os.getenv("ADMIN_IDS", "").split(",")
+    if admin_id.strip()
+]
+
+CHANNEL_ID = os.getenv("CHANNEL_ID")
+
+DB_PATH = os.getenv(
+    "DB_PATH",
+    "stories.db"
+)
+
+
+if not BOT_TOKEN:
+    raise ValueError(
+        "Не найден BOT_TOKEN в .env"
+    )
+
+
+if not ADMIN_IDS:
+    raise ValueError(
+        "Не найден ADMIN_IDS в .env"
+    )
+
+
+if not CHANNEL_ID:
+    raise ValueError(
+        "Не найден CHANNEL_ID в .env"
+    )
