3c1c83842e5043679dfe9558442b668fa584d4a2
Ahaha111a <arkariys777@gmail.com>
2026-08-07 21:08:50 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()  # Загружает переменные из .env
 class Config:
     BOT_TOKEN: str = os.getenv("BOT_TOKEN")
     DB_PATH: str = "stories.db"
-    CHANNEL_ID: int = int(os.getenv("CHANNEL_ID", 0))  # ID канала для публикаций (положи туда свой)
+    CHANNEL_ID: int = 0
     ADMIN_IDS: list[int] = [int(x) for x in os.getenv("ADMIN_IDS", "").split(",") if x]
 
 config = Config()
