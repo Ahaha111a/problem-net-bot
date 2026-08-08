70f049b8167d0c3e60d3ce46cc479097ae89f3bc
Ahaha111a <arkariys777@gmail.com>
2026-08-08 18:55:11 +0300

    Update states.py

--- states.py
+++ states.py
@@ -4,3 +4,4 @@ from aiogram.fsm.state import State, StatesGroup
 class StoryState(StatesGroup):
     waiting_for_story = State()
     waiting_for_edit = State()
+    waiting_for_contact_message = State()
