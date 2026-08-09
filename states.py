df0ac37560fa4fc7b811a8a11b571001943c07cf
Ahaha111a <arkariys777@gmail.com>
2026-08-09 15:54:32 +0300

    Update states.py

--- states.py
+++ states.py
@@ -5,15 +5,14 @@ class StoryState(StatesGroup):
     waiting_for_story = State()
     waiting_for_edit = State()
 
-    # Старый контакт по истории
     waiting_for_contact_message = State()
-
-    # Старый ответ на поддержку
     waiting_for_support_reply = State()
 
-    # Диалог пользователь ↔ модератор
+    # Пользовательский диалог с модератором
     waiting_for_support_message = State()
-    moderator_dialog = State()
 
     # Выбор способа экстренной поддержки
     waiting_for_support_method = State()
+
+    # Модератор находится внутри диалога
+    moderator_dialog = State()
