febfe9cc47a7aacab2b027ec82d3140777a22c62
Ahaha111a <arkariys777@gmail.com>
2026-08-09 15:45:54 +0300

    Update states.py

--- states.py
+++ states.py
@@ -4,7 +4,16 @@ from aiogram.fsm.state import State, StatesGroup
 class StoryState(StatesGroup):
     waiting_for_story = State()
     waiting_for_edit = State()
+
+    # Старый контакт по истории
     waiting_for_contact_message = State()
-    waiting_for_support_message = State()
+
+    # Старый ответ на поддержку
     waiting_for_support_reply = State()
+
+    # Диалог пользователь ↔ модератор
+    waiting_for_support_message = State()
     moderator_dialog = State()
+
+    # Выбор способа экстренной поддержки
+    waiting_for_support_method = State()
